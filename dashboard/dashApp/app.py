b0b090c30e6ca970d14438c20c06b8469775a27e
Lela Tong Jiang Bones <ltb3@hosetower.el.nist.gov>
2018-08-09 11:12:13 -0400

    rnns

--- dashboard/dashApp/app.py
+++ dashboard/dashApp/app.py
@@ -4,32 +4,62 @@ import dash_core_components as dcc
 import dash_html_components as html
 from pathlib import Path
 import pandas as pd
+from dash.dependencies import Input, Output
 
 app = dash.Dash()
 
 data_dir = Path('..') / 'data'
 df = pd.read_csv(data_dir / 'MWOs_anon.csv')
 
+machine_indicators = df['mach'].unique()
+technician_indicators = df['tech'].unique()
+
+app = dash.Dash(__name__)
+
+all_options = {
+    'Technicians': ['New York City', 'San Francisco', 'Cincinnati'],
+    'Machines': [u'Montréal', 'Toronto', 'Ottawa']
+}
 app.layout = html.Div([
-    html.Label('Multi-Select Dropdown'),
-    dcc.Dropdown(
-        options=[
-            {'label': 'New York City', 'value': 'NYC'},
-            {'label': u'Montréal', 'value': 'MTL'},
-            {'label': 'San Francisco', 'value': 'SF'}
-        ],
-        value=['MTL', 'SF'],
-        multi=True
+    dcc.RadioItems(
+        id='countries-dropdown',
+        options=[{'label': k, 'value': k} for k in all_options.keys()],
+        value='America'
     ),
 
-    html.Label('Slider'),
-    dcc.Slider(
-        min=0,
-        max=9,
-        marks={i: 'Label {}'.format(i) if i == 1 else str(i) for i in range(1, 6)},
-        value=5,
-    ),
-], style={'columnCount': 2})
+    html.Hr(),
+
+    dcc.RadioItems(id='cities-dropdown'),
+
+    html.Hr(),
+
+    html.Div(id='display-selected-values')
+])
+
+
+@app.callback(
+    dash.dependencies.Output('cities-dropdown', 'options'),
+    [dash.dependencies.Input('countries-dropdown', 'value')])
+def set_cities_options(selected_country):
+    return [{'label': i, 'value': i} for i in all_options[selected_country]]
+
+
+@app.callback(
+    dash.dependencies.Output('cities-dropdown', 'value'),
+    [dash.dependencies.Input('cities-dropdown', 'options')])
+def set_cities_value(available_options):
+    return available_options[0]['value']
+
+
+@app.callback(
+    dash.dependencies.Output('display-selected-values', 'children'),
+    [dash.dependencies.Input('countries-dropdown', 'value'),
+     dash.dependencies.Input('cities-dropdown', 'value')])
+def set_display_children(selected_country, selected_city):
+    return u'{} is a city in {}'.format(
+        selected_city, selected_country,
+    )
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
